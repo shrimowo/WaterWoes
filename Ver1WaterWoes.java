d648fba955362d6af82aac505f17351caa41ce26
emery soup <MY_NAME@example.com>
2024-07-31 16:02:14 +1200

    organised it a little

--- Ver1WaterWoes.java
+++ Ver1WaterWoes.java
@@ -8,7 +8,7 @@ import java.awt.geom.*;
 import java.util.Scanner; //Keyboard input
   // Gnomenu |BAZINGA| \\
     // Coding a menu 14/02/2024 \\
-public class WaterWoes extends JFrame implements ActionListener, MouseListener{
+public class Ver1WaterWoes extends JFrame implements ActionListener, MouseListener{
     final int GRIDSIZE = 10;
     boolean StartScreen = true;
     boolean GridGenerating = true;
@@ -18,64 +18,6 @@ public class WaterWoes extends JFrame implements ActionListener, MouseListener{
     JMenuItem menuItem;
     Canvas myGraphic;
     Graphics fixer;
-    public WaterWoes()
-    {
-        setTitle("Wellington Water");
-        this.getContentPane().setPreferredSize(new Dimension(1055,1055));
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.pack();
-        this.toFront();
-        this.setVisible(true);
-        
-        WaterWoesText test = new WaterWoesText("Do You Want A Guide To Water-Works?");
-        test.setLocationRelativeTo(this);
-        test.setVisible(true);
-        String reply=test.getText();
-        System.out.println(reply);
-        
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(504,1000));
-        myGraphic = new Canvas();
-        panel.add(myGraphic);
-        
-        addMouseListener(this);
-        
-        menuBar=new JMenuBar();
-        this.setJMenuBar(menuBar);
-        
-        menu = new JMenu("Infrastructure Options");
-        menuBar.add(menu);
-        
-        menuItem=new JMenuItem("Water Source");
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke('w'));
-        menu.add(menuItem);
-        this.pack();
-        
-        menuItem=new JMenuItem("Sink");
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke('s'));
-        menu.add(menuItem);
-        this.pack();
-        
-        menuItem=new JMenuItem("Pipe");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke('p'));
-        menuItem.addActionListener(this);
-        menu.add(menuItem);
-        this.pack();    
-        
-        menuItem=new JMenuItem("Junction");
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke('j'));
-        menu.add(menuItem);
-        this.pack();  
-        
-        menuItem=new JMenuItem("QUIT");
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke('q'));
-        menu.add(menuItem);
-        this.pack();  
-    }
     public void mouseExited(MouseEvent e) {} //{System.out.println("exit");}
     public void mouseEntered(MouseEvent e) {}//{System.out.println("enter");}
     public void mouseReleased(MouseEvent e) {}//{System.out.println("release");}
@@ -86,7 +28,11 @@ public class WaterWoes extends JFrame implements ActionListener, MouseListener{
         System.out.println("Will place here:  x:"+(mousex)+" y:"+(mousey));
     }
     public void paint (Graphics g) {
-        g.drawLine(0, 100, 0, 0);
+        final String fileName="sewer.JPG";
+        ImageIcon image= new ImageIcon(fileName);
+        g.drawLine(0, 0, 100, 100);
+        super.paint(g);
+        image.paintIcon(this,g,0,550);
     }
     void createDialog(){
         JDialog box = new JDialog(this);
@@ -101,7 +47,7 @@ public class WaterWoes extends JFrame implements ActionListener, MouseListener{
     public void createWaterSource (Graphics g){
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawLine(100,100,100,100);
+        g2.drawOval(150,450,200,500);
     }
     public void actionPerformed(ActionEvent e){
         String cmd=e.getActionCommand();
@@ -282,5 +228,63 @@ public class WaterWoes extends JFrame implements ActionListener, MouseListener{
         }
     }
     */
- 
+    public Ver1WaterWoes()
+    {
+        
+        setTitle("Wellington Water");
+        this.getContentPane().setPreferredSize(new Dimension(1055,1055));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.toFront();
+        this.setVisible(true);
+        
+        WaterWoesText test = new WaterWoesText("Do You Want A Guide To Water-Works?");
+        test.setLocationRelativeTo(this);
+        test.setVisible(true);
+        String reply=test.getText();
+        System.out.println(reply);
+        
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(504,1000));
+        myGraphic = new Canvas();
+        panel.add(myGraphic);
+        
+        addMouseListener(this);
+        
+        menuBar=new JMenuBar();
+        this.setJMenuBar(menuBar);
+        
+        menu = new JMenu("Infrastructure Options");
+        menuBar.add(menu);
+        
+        menuItem=new JMenuItem("Water Source");
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke('w'));
+        menu.add(menuItem);
+        this.pack();
+        
+        menuItem=new JMenuItem("Sink");
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke('s'));
+        menu.add(menuItem);
+        this.pack();
+        
+        menuItem=new JMenuItem("Pipe");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke('p'));
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+        this.pack();    
+        
+        menuItem=new JMenuItem("Junction");
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke('j'));
+        menu.add(menuItem);
+        this.pack();  
+        
+        menuItem=new JMenuItem("QUIT");
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke('q'));
+        menu.add(menuItem);
+        this.pack();  
+    }
 }
